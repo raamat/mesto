6b59f0d7f673e235aefd4bde27e43a8e96a90673
Raamat <raamat@bk.ru>
2022-12-23 13:33:00 +0300

    refactor: переписать функции для открытия и закрытия popup

--- scripts/index.js
+++ scripts/index.js
@@ -4,49 +4,49 @@ const profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 const profileEditButton = document.querySelector('.profile__edit-button');
 
-//Все модальные окна в одной переменной popup
-const popup = document.querySelectorAll('.popup');
-
-//Все кнопки закрытия модальных окон в одной переменной popupCloseButton
-const popupCloseButton = document.querySelectorAll('.popup__close-button');
+//Коллекция со всеми модальными окнами
+const popupsList = document.querySelectorAll('.popup');
 
 let popupFieldName = document.querySelector('.popup__field_type_name');
 let popupFieldJob = document.querySelector('.popup__field_type_job');
 const popupForm = document.querySelector('.popup__form');
-const popupEditProfile =document.querySelector('.popup_type_edit-profile');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 
 /********************************************* Переменные для карточек *********************************************/
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.cards__list');
-const card = document.querySelector('.card');
+//const card = document.querySelector('.card');
 const profileAddButton = document.querySelector('.profile__add-button');
 const popupAddCard = document.querySelector('.popup_type_add-card');
 let popupFieldPlace = document.querySelector('.popup__field_type_place');
 let popupFieldLink = document.querySelector('.popup__field_type_link');
-const popupCreateButton = document.querySelector('.popup__submit-button_type_create-card');
+//const popupCreateButton = document.querySelector('.popup__submit-button_type_create-card');
 const popupFormAddCard = document.querySelector('.popup__form_type_add-card');
 
-popupTypeZoomPhoto = document.querySelector('.popup_type_zoom-photo');
-popupPhoto = document.querySelector('.popup__photo');
-popupPhotoCaption = document.querySelector('.popup__photo-caption');
+popupZoomPhoto = document.querySelector('.popup_type_zoom-photo');
+popupPhoto = popupZoomPhoto.querySelector('.popup__photo');
+popupPhotoCaption = popupZoomPhoto.querySelector('.popup__photo-caption');
 
 /*********************************************** Функции ******************************************************/
 /**************************************************************************************************************/
 
-//Функция открытия модального окна редактирования профиля - становится видимым модальное окно за счет добавления класса popup_opened
-function profileEdit(event) {
-  popupEditProfile.classList.add('popup_opened');
+//Универсальная функция открытия модального окна - становится видимым модальное окно за счет добавления класса popup_opened
+function openPopup(template) {
+  template.classList.add('popup_opened');
+}  
+
+//Функция открытия модального окна редактирования профиля
+function editProfile() {
   popupFieldName.value = profileTitle.textContent;
   popupFieldJob.value = profileSubtitle.textContent;
+
+  openPopup(popupEditProfile);
 }
 
-//Функция закрытия модального окна
-function popupClose(event) {
-  //popup.classList.remove('popup_opened');
-  popup.forEach(function(element) {
-    element.classList.remove('popup_opened');
-  })
+//Универсальная функция закрытия модального окна
+function closePopup(template) {
+  template.classList.remove('popup_opened');
 }
 
 //Сохранение данных из формы редактирования профиля
@@ -54,9 +54,11 @@ function saveForm(event) {
   /*Если наше событие находится в переменной event, то для предотвращения поведения по умолчанию (отправлять данные самостоятельно) 
   мы можем вызвать event.preventDefault() https://doka.guide/js/deal-with-forms */
   event.preventDefault();
+
   profileTitle.textContent = popupFieldName.value;
   profileSubtitle.textContent = popupFieldJob.value;
-  popupClose();
+  
+  closePopup(popupEditProfile);
 }
 
 /**************************************** Работа с карточками ***************************************/
@@ -65,12 +67,13 @@ function saveForm(event) {
 function createCard(popupFieldPlace, popupFieldLink) {
   //Ниже получаем содержимое контейнера li - "лишки", класс card
   const cardTemplateClone = cardTemplate.querySelector('.card').cloneNode(true);
+
   const cardPhoto = cardTemplateClone.querySelector('.card__photo');
   cardTemplateClone.querySelector('.card__title').textContent = popupFieldPlace; 
   cardPhoto.src = popupFieldLink;
   cardPhoto.alt = popupFieldPlace;
 
-   //Удаление карточки
+  //Удаление карточки
   cardTemplateClone.querySelector('.card__delete-button').addEventListener('click', () => {
     cardTemplateClone.remove();
 });
@@ -82,7 +85,7 @@ function createCard(popupFieldPlace, popupFieldLink) {
    
   //Слушатель увеличения картинки
   cardTemplateClone.querySelector('.card__photo').addEventListener('click', () => {
-    zoomPhoto(cardTemplateClone);
+    zoomPhoto(popupFieldLink, popupFieldPlace);
   });
 
   return cardTemplateClone;
@@ -94,14 +97,14 @@ initialCards.forEach(function(element) {
 })
 
 //Функция открытия модального окна карточки - становится видимым модальное окно за счет добавления класса popup_opened
-function cardFormOpened(event) {
-  popupAddCard.classList.add('popup_opened');
+function openCardForm(event) {
+  openPopup(popupAddCard);
 }
 
 //Функция добавления одной карточки
-function cardAdd(event) {
+function addCard(event) {
   event.preventDefault();
-  
+
   const placeName = popupFieldPlace.value;
   const placeLink = popupFieldLink.value;
 
@@ -110,16 +113,17 @@ function cardAdd(event) {
   popupFieldPlace.value = '';
   popupFieldLink.value = '';
 
-  popupClose();
+  closePopup(popupAddCard);
 }
 
 //Функция увеличения картинок
-function zoomPhoto(template) {
-  const cardCaption = template.querySelector('.card__caption').textContent;
-  popupTypeZoomPhoto.classList.add('popup_opened');
-  popupPhoto.src = template.querySelector('.card__photo').src;
-  popupPhotoCaption.textContent = cardCaption;
-  popupPhoto.alt = cardCaption;
+function zoomPhoto(src, caption) {
+  popupPhoto.src = src;
+  popupPhoto.alt = caption;
+
+  popupPhotoCaption.textContent = caption;
+
+  openPopup(popupZoomPhoto);
 }
 
 /**************************************************** Слушатели вне функций **************************************************/
@@ -129,15 +133,18 @@ function zoomPhoto(template) {
 popupForm.addEventListener('submit', saveForm);
 
 //Слушатель события клик по кнопке "Редактировать"
-profileEditButton.addEventListener('click', profileEdit);
+profileEditButton.addEventListener('click', editProfile);
 
-//Cлушатель события клик по кнопке "Закрыть"*/
-popupCloseButton.forEach((element) => {
-  element.addEventListener('click', popupClose);
-}) 
+//Подвешиваем слушатель события клик по кнопке "Закрыть" на каждое окно
+popupsList.forEach((popup) => {
+  const closeButton = popup.querySelector(".popup__close-button");
+  closeButton.addEventListener("click", () => {
+    closePopup(popup);
+  });
+});
 
 //Слушатель события клик по кнопке "Добавить" карточку
-profileAddButton.addEventListener('click', cardFormOpened);
+profileAddButton.addEventListener('click', openCardForm);
 
 //Слушатель события по кнопке "Создать" карточку
-popupFormAddCard.addEventListener('submit', cardAdd);
+popupFormAddCard.addEventListener('submit', addCard);
